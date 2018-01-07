a13b2b740c42b6eb426067eb3855be7b4a944c76
Konstantin Fukszon <fukszon.kostya@gmail.com>
2018-01-06 16:05:02 -0800

    Added some more files

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,21 +1,50 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
+hbs.registerPartials(__dirname +'/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next)=>{
+    var now = new Date().toString();
+    var log = `${now} - ${req.method} - ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err)=>{
+        if (err) console.log('Unabel to append to server.log');
+    });
+    next();
+});
+app.use((req, res, next)=>{
+    res.render('maintenance.hbs');
+});
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', ()=>{
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text)=>{
+    return  text.toUpperCase();
+});
+
 app.get('/', (req, res)=>{
-    //res.send('<h1>hello Express!</h1>');
-    res.send({
-        name: 'Konstantin',
-        likes:['biking', 'riding']
+    res.render('home.hbs',{
+        pageTitle: 'Home Page - from the partial',
+        welcomeMessage: 'Hello, how are you?'
     });
 });
 app.get('/about', (req, res)=>{
-    res.send('It is me. ABout page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
 });
 
 app.get('/bad', (req, res)=>{
-    var e = new Error();
-    res.send(e);
+    res.send({
+        errorMessage: "Unable to hande request"
+    });
 });
 
-app.listen(3000);
+app.listen(3000, ()=>{
+    console.log('Server is up on port 3000');
+});
